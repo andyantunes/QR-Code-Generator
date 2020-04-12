159fd9da8c1435116af07715ae59157458a61130
unknown <andersonlantunes@gmail.com>
2020-04-12 15:04:56 -0300

    Deleted files and get cdn

--- js/tabs.js
+++
@@ -1,4 +0,0 @@
-$('#myTab a[href="#perfil"]').tab('show') // Por ID
-$('#myTab li:first-child a').tab('show') // Ativar o primeiro elemento filho
-$('#myTab li:last-child a').tab('show') // Ativar o último elemento filho
-$('#myTab li:nth-child(3) a').tab('show') // Selecionar um filho específico (terceiro, neste caso)
